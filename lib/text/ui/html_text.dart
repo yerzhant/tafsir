3b0dec7da33ab3f377eb4c52699e5d4b6ba25490
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2020-07-12 05:59:50 +0600

    Add html text widget

---
+++ lib/text/ui/html_text.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter_plus/webview_flutter_plus.dart';
+
+class HtmlText extends StatefulWidget {
+  final String text;
+
+  const HtmlText({Key key, this.text}) : super(key: key);
+
+  @override
+  _HtmlTextState createState() => _HtmlTextState();
+}
+
+class _HtmlTextState extends State<HtmlText> {
+  WebViewPlusController _controller;
+
+  var _height = 1.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: _height,
+      child: WebViewPlus(
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (controller) {
+          _controller = controller;
+          controller.loadString(widget.text + _style);
+        },
+        onPageFinished: (url) async {
+          var height = await _controller.getHeight();
+          setState(() {
+            _height = height;
+          });
+        },
+      ),
+    );
+  }
+}
+
+const _style = '''
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body {
+    line-height: 1.35;
+    color: #373a3c;
+    overflow-wrap: break-word;
+    word-wrap: break-word;
+    word-break: break-word;
+    background-color: #fafafa;
+}
+blockquote.blue {
+    padding: 10px 15px;
+    background-color: #f5f5f5;
+    border-left: 3px solid #0088c7;
+}
+ul, ol {
+    margin-left: 16px;
+}
+ul.blue {
+    list-style: none;
+}
+ul.blue li::before {
+    content: '\\2022';
+    color: #0088c7;
+    font-weight: bold;
+    display: inline-block;
+    width: 1em;
+    margin-left: -1em;
+}
+a {
+    text-decoration: none;
+    color: #6699cc;
+}
+.snoski {
+    padding: 10px;
+    background-color: #eef5f7;
+}
+.specdiv {
+    background-color: #f5f5f5;
+    border: 1px solid #e8e8e8;
+    padding: 10px;
+}
+</style>
+''';
--- lib/text/ui/surah_info.dart
+++ lib/text/ui/surah_info.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:tafsir/constants.dart';
 import 'package:tafsir/suwar/model/surah.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'package:webview_flutter_plus/webview_flutter_plus.dart';
+import 'package:tafsir/text/ui/html_text.dart';
 
 const _server = 'https://azan.ru';
 const _images = '$_server/media/images/surahs';
-
-const _padding = 15.0;
 
 var _surahTitleStyle = TextStyle(
   fontSize: 20,
@@ -17,72 +14,44 @@ var _surahTitleStyle = TextStyle(
 
 const _secondaryTextStyle = TextStyle(color: Colors.grey);
 
-class SurahInfo extends StatefulWidget {
+class SurahInfo extends StatelessWidget {
   final Surah surah;
 
   SurahInfo({Key key, this.surah}) : super(key: key);
 
   @override
-  _SurahInfoState createState() => _SurahInfoState();
-}
-
-class _SurahInfoState extends State<SurahInfo> {
-  var _descHeight = 1.0;
-  WebViewPlusController _webViewPlusController;
-
-  @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Image.network('$_images/${widget.surah.image}'),
+        Image.network('$_images/${surah.image}'),
         SizedBox(height: 25),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(widget.surah.weight.toString(), style: _surahTitleStyle),
+            Text(surah.weight.toString(), style: _surahTitleStyle),
             SizedBox(width: 10),
             Text(
-              'Сура «${widget.surah.title.toUpperCase()}»',
+              'Сура «${surah.title.toUpperCase()}»',
               style: _surahTitleStyle,
             ),
           ],
         ),
         SizedBox(height: 7),
-        Text('«${widget.surah.titleInRussian}»', style: _secondaryTextStyle),
+        Text('«${surah.titleInRussian}»', style: _secondaryTextStyle),
         SizedBox(height: 10),
         Padding(
-          padding: EdgeInsets.all(_padding),
+          padding: EdgeInsets.all(15),
           child: Column(
             children: <Widget>[
-              _lineWithButton(widget.surah.revealAt == 'Medina'
+              _lineWithButton(surah.revealAt == 'Medina'
                   ? 'Мединская сура'
                   : 'Мекканская сура'),
-              _lineWithButton('${widget.surah.ayatsCount} аятов'),
-              _lineWithButton('${widget.surah.dzhuz} джуз'),
-              _lineWithButton(
-                  'в порядке ниспосылания - ${widget.surah.revealOrder}'),
+              _lineWithButton('${surah.ayatsCount} аятов'),
+              _lineWithButton('${surah.dzhuz} джуз'),
+              _lineWithButton('в порядке ниспосылания - ${surah.revealOrder}'),
+              Divider(height: 35),
+              HtmlText(text: surah.text),
             ],
-          ),
-        ),
-        Divider(height: 0),
-        Padding(
-          padding: const EdgeInsets.all(_padding),
-          child: SizedBox(
-            height: _descHeight,
-            child: WebViewPlus(
-              javascriptMode: JavascriptMode.unrestricted,
-              onWebViewCreated: (controller) {
-                _webViewPlusController = controller;
-                controller.loadString(
-                    widget.surah.text + '<style>div {color: red;} </style>');
-              },
-              onPageFinished: (url) async {
-                var height = await _webViewPlusController.getHeight();
-                setState(() {
-                  _descHeight = height;
-                });
-              },
-            ),
           ),
         ),
       ],
