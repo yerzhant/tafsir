a3aad9631e718e51860538df7eabded6ef6ca7fe
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2020-08-07 05:34:02 +0600

    Change colors

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
-// const primaryColor = const Color(0xff2471b1);
-// const primaryColor = const Color(0xff1f3068);
-const primaryColor = const Color(0xff263b7c);
-// const primaryColor = const Color(0xff1a5591);
+const primaryColor = Colors.indigoAccent;
+// const primaryColor = const Color(0xff263b7c);
 
-const textColor = const Color(0xFF616161);
+const textColor = const Color(0xFF414141);
 const textColorGrey = Colors.grey;
 
 const padding = 16.0;
