1b4c67f72817137f93aaeb7f427aefab62431ea5
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-06-12 12:25:21 +0600

    Add tafsir db

---
+++ lib/common/ds/tafsir_db.dart
@@ -0,0 +1,75 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:tafsir/suwar/domain/model/surah.dart';
+
+const _surahTableName = 'surah';
+
+class TafsirDB {
+  late Database db;
+
+  Future<List<Map<String, dynamic>>> getAllSuwar() {
+    return db.query(_surahTableName, orderBy: 'weight');
+  }
+
+  Future<void> _insert(Surah surah) async {
+    await db.insert(_surahTableName, surah.toMap());
+  }
+
+  void insertAll(List<Surah> suwar) {
+    for (final s in suwar) {
+      _insert(s);
+    }
+  }
+
+  Future<void> init() async {
+    db = await openDatabase(
+      'app.db',
+      version: 2,
+      onCreate: (db, _) async {
+        await db.execute('''
+          create table surah(
+            id integer primary key,
+            weight integer not null,
+            title text not null,
+            title_in_russian text,
+            text text,
+            ayats_count integer,
+            dzhuz text,
+            reveal_at text,
+            reveal_order integer,
+            image text,
+            slug text
+          )
+        ''');
+
+        await db.execute('''
+          create table aayah(
+            id integer primary key,
+            text_origin text not null,
+            surah_id integer not null,
+            weight integer not null,
+            title text not null,
+            text text not null,
+            tafsir text
+          )
+        ''');
+
+        await db.execute('''
+          create table bookmark(
+            id integer primary key autoincrement,
+            surah_id integer not null,
+            surah_title text not null,
+            aayah integer not null
+          )
+        ''');
+      },
+      onUpgrade: (db, oldVersion, newVersion) async {
+        if (oldVersion == 1) {
+          await db.execute('alter table surah add slug text');
+          await db.delete('surah');
+        }
+      },
+    );
+  }
+
+  Future<void> close() => db.close();
+}
