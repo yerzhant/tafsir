70aa317cbcfc8e61b4ddd53a297d48f2d6809be5
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-06-13 06:24:32 +0600

    Add divider to text widget

--- lib/text/ui/widget/text_widget.dart
+++ lib/text/ui/widget/text_widget.dart
@@ -24,17 +24,47 @@ class _TextWidgetState extends State<TextWidget> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.end,
       children: <Widget>[
+        _divider(),
         Text(
           widget.textItem.textOrigin.trim(),
           textAlign: TextAlign.start,
           textDirection: TextDirection.rtl,
           style: GoogleFonts.scheherazade(
             fontSize: 30,
+            height: 1.3,
             color: Theme.of(context).primaryColor,
           ),
         ),
+        const SizedBox(height: 7),
         _translation(),
         if (widget.textItem.tafsir.isNotEmpty) ..._tafsir(),
+      ],
+    );
+  }
+
+  Row _divider() {
+    return Row(
+      children: [
+        Container(
+          width: 30,
+          height: 30,
+          decoration: BoxDecoration(
+            border: Border.all(color: const Color(0x4dBDBDC2)),
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+          ),
+          child: Center(
+            child: Text(
+              widget.textItem.title,
+              style: GoogleFonts.lato(
+                fontSize: 13,
+                fontWeight: FontWeight.w800,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(width: 11),
+        const Expanded(child: Divider(height: 1)),
       ],
     );
   }
