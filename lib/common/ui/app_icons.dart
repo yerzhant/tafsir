82721b8c8510433ca749b1d44bd3350242231ed9
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-06-13 16:10:29 +0600

    Add app icons

---
+++ lib/common/ui/app_icons.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+
+class AppIcons {
+  AppIcons._();
+
+  static const String _fontFamily = 'AppIcons';
+
+  static const IconData moreHorizontal =
+      IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData fontSizes = IconData(0xe902, fontFamily: _fontFamily);
+  static const IconData theme = IconData(0xe903, fontFamily: _fontFamily);
+  static const IconData close = IconData(0xe904, fontFamily: _fontFamily);
+  static const IconData home = IconData(0xe901, fontFamily: _fontFamily);
+}
